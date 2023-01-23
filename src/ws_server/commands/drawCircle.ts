f434367a516d4b1c9a7d16848dff97ef1b7a7e8d
ipanfilenko <panfilenko.igor@gmail.com>
2023-01-23 17:42:50 +0300

    feat: draw circle

--- src/ws_server/commands/drawCircle.ts
+++ src/ws_server/commands/drawCircle.ts
@@ -1,8 +1,24 @@
-import { mouse } from '@nut-tree/nut-js';
+import { mouse, straightTo, Button, Point } from '@nut-tree/nut-js';
 
 export const drawCircle = async (commandType: string, args: string[]) => {
-    const [ radius ] = args; 
+    let [ radius = 0 ] = args; 
     const { x, y } = await mouse.getPosition();
-    console.log('Circle', x, y, radius);
+    
+    radius = Number(radius);
+    const step = 0.01;
+
+	for (let i = 0; i <= 2 * Math.PI; i = i + step) {
+		const moveX = x - radius * Math.cos(i);
+		const moveY = y - radius * Math.sin(i);
+
+		await mouse.move(straightTo(new Point(moveX, moveY)));
+
+        if (i === 0) {
+            await mouse.pressButton(Button.LEFT);
+        }
+	}
+
+	await mouse.releaseButton(Button.LEFT);
+
     return commandType;
 }
