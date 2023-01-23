cddff81c9657713c22833ef7ef19965f07390d7b
ipanfilenko <panfilenko.igor@gmail.com>
2023-01-23 12:18:35 +0300

    feat: add typescript

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from "./src/http_server/index";
 import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
-
+console.log(mouse);
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
